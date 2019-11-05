3014145de8aaf1a77f181c4fd84e6d0f3ab94435
亚超 杨 <yangyachao@live.cn>
2019-11-05 07:29:33 +0000

    version

--- Versioning/Versioning/Program.cs
+++ Versioning/Versioning/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Versioning
@@ -14,12 +9,14 @@ namespace Versioning
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
     }
 }
--- Versioning/Versioning/Startup.cs
+++ Versioning/Versioning/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,26 +18,30 @@ namespace Versioning
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers().AddNewtonsoftJson();
 
             services.AddApiVersioning(option =>
             {
-                option.ReportApiVersions  = true;
+                option.ReportApiVersions = true;
                 //option.ApiVersionReader = new HeaderApiVersionReader("api-version");
-                option.ApiVersionReader   = ApiVersionReader.Combine
+                option.ApiVersionReader = ApiVersionReader.Combine
                 (
                     new QueryStringApiVersionReader("api-version"),
                     new HeaderApiVersionReader("api-version")
                 );
                 option.AssumeDefaultVersionWhenUnspecified = true;
-                option.DefaultApiVersion                   = new ApiVersion(1, 0);
+                option.DefaultApiVersion = new ApiVersion(1, 0);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
